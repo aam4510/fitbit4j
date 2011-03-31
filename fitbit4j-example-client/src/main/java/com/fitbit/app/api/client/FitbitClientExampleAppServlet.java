b90d7fe7837db8d4f016007367066482cd7748a4
unknown <Kiryl Kazakevich@.(none)>
2011-04-01 00:29:42 +0300

    fitbit api example extracting

---
+++ fitbit4j-example-client/src/main/java/com/fitbit/app/api/client/FitbitClientExampleAppServlet.java
@@ -0,0 +1,11 @@
+package com.fitbit.app.api.client;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Kiryl Kazakevich
+ * Date: 3/30/11
+ * Time: 8:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FitbitClientExampleAppServlet {
+}
---
+++ fitbit4j-example-client/src/main/java/com/fitbit/app/api/client/context/RequestContext.java
@@ -0,0 +1,11 @@
+package com.fitbit.app.api.client.context;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Kiryl Kazakevich
+ * Date: 3/31/11
+ * Time: 8:46 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RequestContext {
+}
