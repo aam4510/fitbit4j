3db514c5755beb77d6eb3fe5cfcc60bf743e3b80
Alexander Pakulov <a.pakulov@gmail.com>
2011-05-19 17:53:38 +0300

    SITE-5766 - API: Automate new API jar release process

---
+++ push-repository-updates.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add .
+git commit -a -m 'Generating website repository.'
+git push origin gh-pages
--- update-directory-index.sh
+++ update-directory-index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-for DIR in $(find ./repository -type d); do
+for DIR in $(find . -type d \( -name repository -o -name release -o -name snapshot \)); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
