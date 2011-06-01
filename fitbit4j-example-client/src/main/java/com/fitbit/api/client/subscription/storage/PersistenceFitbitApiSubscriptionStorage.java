6c83f49056c1259c828ff9e82a9078d7c94dd384
unknown <Kiryl@.(none)>
2011-06-01 14:11:54 +0300

    SITE-6364 - Example application does not display existing subscriptions when view subscriptions chosen.

--- fitbit4j-example-client/src/main/java/com/fitbit/api/client/subscription/storage/PersistenceFitbitApiSubscriptionStorage.java
+++ fitbit4j-example-client/src/main/java/com/fitbit/api/client/subscription/storage/PersistenceFitbitApiSubscriptionStorage.java
@@ -3,32 +3,42 @@ package com.fitbit.api.client.subscription.storage;
 import com.fitbit.api.client.FitbitApiSubscriptionStorage;
 import com.fitbit.api.client.LocalSubscriptionDetail;
 import com.fitbit.api.client.subscription.dao.ISubscriptionDao;
+import com.fitbit.api.client.subscription.model.Subscription;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@Transactional
 public class PersistenceFitbitApiSubscriptionStorage implements FitbitApiSubscriptionStorage {
 
     @Resource
     private ISubscriptionDao subscriptionDao;
 
     @Override
-    public void save(LocalSubscriptionDetail subscription) {
-        //To change body of implemented methods use File | Settings | File Templates.
+    public void save(LocalSubscriptionDetail localSubscription) {
+        Subscription subscription = subscriptionDao.findSingleByQuery("FROM Subscription s WHERE s.subscriptionId = ?", localSubscription.getSubscriptionDetail().getSubscriptionId());
+        if (subscription == null) {
+            subscriptionDao.persist(SubscriptionAdaptor.toSubscription(localSubscription));
+        }
     }
 
     @Override
     public LocalSubscriptionDetail getBySubscriptionId(String subscriptionId) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        Subscription subscription = subscriptionDao.findSingleByQuery("FROM Subscription s WHERE s.subscriptionId = ?", subscriptionId);
+        return subscription != null ? SubscriptionAdaptor.toLocalSubscriptionDetails(subscription) : null;
     }
 
     @Override
-    public void delete(LocalSubscriptionDetail subscription) {
-        //To change body of implemented methods use File | Settings | File Templates.
+    public void delete(LocalSubscriptionDetail localSubscription) {
+        Subscription subscription = subscriptionDao.findSingleByQuery("FROM Subscription s WHERE s.subscriptionId = ?", localSubscription.getSubscriptionDetail().getSubscriptionId());
+        if (subscription != null) {
+            subscriptionDao.remove(subscription);
+        }
     }
 
     @Override
     public List<LocalSubscriptionDetail> getAllSubscriptions() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+        return SubscriptionAdaptor.toLocalSubscriptionDetailsList(subscriptionDao.findAll());
     }
 }
---
+++ fitbit4j-example-client/src/main/java/com/fitbit/api/client/subscription/storage/SubscriptionAdaptor.java
@@ -0,0 +1,42 @@
+package com.fitbit.api.client.subscription.storage;
+
+import com.fitbit.api.client.LocalSubscriptionDetail;
+import com.fitbit.api.client.subscription.model.Subscription;
+import com.fitbit.api.model.FitbitResourceOwner;
+import com.fitbit.api.model.SubscriptionDetail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class SubscriptionAdaptor {
+
+    private SubscriptionAdaptor() {
+    }
+
+    public static LocalSubscriptionDetail toLocalSubscriptionDetails(Subscription subscription) {
+        SubscriptionDetail subscriptionDetail = new SubscriptionDetail(subscription.getSubscriberId(), subscription.getSubscriptionId(), FitbitResourceOwner.fromIdentifier(subscription.getResourceOwnerType(), subscription.getResourceOwnerId()), subscription.getCollectionType());
+        LocalSubscriptionDetail localSubscriptionDetail = new LocalSubscriptionDetail(subscriptionDetail, subscription.isKnownSubscription());
+        localSubscriptionDetail.setLastUpdateNotificationDate(subscription.getLastUpdateNotificationDate());
+        return localSubscriptionDetail;
+    }
+
+    public static List<LocalSubscriptionDetail> toLocalSubscriptionDetailsList(List<Subscription> subscriptions) {
+        List<LocalSubscriptionDetail> detailList = new ArrayList<LocalSubscriptionDetail>(subscriptions.size());
+        for(Subscription subscription : subscriptions) {
+            detailList.add(toLocalSubscriptionDetails(subscription));
+        }
+        return detailList;
+    }
+
+    public static Subscription toSubscription(LocalSubscriptionDetail localSubscriptionDetail) {
+        Subscription subscription = new Subscription();
+        subscription.setSubscriptionId(localSubscriptionDetail.getSubscriptionDetail().getSubscriptionId());
+        subscription.setSubscriberId(localSubscriptionDetail.getSubscriptionDetail().getSubscriberId());
+        subscription.setResourceOwnerId(localSubscriptionDetail.getSubscriptionDetail().getOwner().getId());
+        subscription.setResourceOwnerType(localSubscriptionDetail.getSubscriptionDetail().getOwner().getResourceOwnerType());
+        subscription.setCollectionType(localSubscriptionDetail.getSubscriptionDetail().getCollectionType());
+        subscription.setKnownSubscription(localSubscriptionDetail.isKnownSubscription());
+        subscription.setLastUpdateNotificationDate(localSubscriptionDetail.getLastUpdateNotificationDate());
+        return subscription;
+    }
+}
