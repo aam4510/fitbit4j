d80ae080a47e02da767826f09ffc51c4bf68a3c8
Alexander Pakulov <a.pakulov@gmail.com>
2011-06-06 15:53:29 +0300

    SITE-6491 - API should allow for accessing aggregate step, distance and cal burn data for all friends of a user

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-for DIR in $(find . -type d \( -name repository -o -name release -o -name snapshot \)); do
+for DIR in $(find ./repository ./snapshot ./release -type d); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
     ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
